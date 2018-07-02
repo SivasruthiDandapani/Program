3431756b1141fcf010f3483996e07309fc6c987b
SivasruthiDandapani <34651309+SivasruthiDandapani@users.noreply.github.com>
2018-07-02 23:51:59 +0530

    Java Program
    Question is described inside the file.

---
+++ DiagonalElementsOfMatrix.java
@@ -0,0 +1,61 @@
+﻿/*
+
+Printing diagonal of a matrix
+
+I/P
+    1 2 3 4
+    5 6 7 8
+    9 10 11 12
+    
+O/P
+    1
+    2 5
+    3 6 9
+    4 7 10
+    8 11
+    12
+
+
+*/
+
+
+
+import java.util.*;
+
+public class MyClass {
+    public static void main(String args[]) {
+        int row, col, r, c, i, j;
+        Scanner sc = new Scanner(System.in);
+        row = sc.nextInt();
+        col = sc.nextInt();
+        int[][] matrix = new int[row][col];
+        for(i = 0; i < row; i++)
+        {
+            for(j = 0; j < col; j++)
+            {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        
+        //Printing upto first half
+        for(j = 0; j < col; j++)
+        {
+            for(r = 0, c = j; (r!=row-1 && c!=0); r++, c--)
+            {
+                System.out.print(matrix[r][c] + " ");
+            }
+            System.out.print(matrix[r][c] + "\n");
+        }
+        
+        //Print the second half
+        for(i = 1; i < row; i++)
+        {
+            for(r = i, c = col-1; (r!=row-1 && c!=0); r++, c--)
+            {
+                System.out.print(matrix[r][c] + " ");
+            }
+            System.out.print(matrix[r][c] + "\n");
+        }
+        
+    }
+}
---
+++ Priting_3_4_33_34_43_44.java
@@ -0,0 +1,53 @@
+﻿/*
+Form a number system with only 3 and 4. Find the nth number of the number system.
+Eg.) The numbers are: 3, 4, 33, 34, 43, 44, 333, 334, 343, 344, 433, 
+434, 443, 444, 3333, 3334, 3343, 3344, 3433, 3434, 3443, 3444 ….
+*/
+
+
+
+import java.util.*;
+import java.lang.*;
+
+public class Number
+{
+    static int inc = 0, flag = 0;
+    //Call the function recursively to get ech number. Traverse & see the result
+	static void fun(int sum, int power, int n, int n1, int n2, int pos)
+	{
+		int r, pw;
+		if(power == 0)
+		{
+		    inc++;
+		    if(inc == pos)
+		    {
+			    System.out.print(sum+n + " ");
+			    flag = 1;
+			    System.exit(0);
+		    }
+		}
+		else
+		{
+			pw = (int)Math.pow(10, (double)power);
+			r = sum + (n * pw);
+			fun(r, power-1, n1, n1, n2, pos);
+			fun(r, power-1, n2, n1, n2, pos);
+		}
+	}
+	public static void main(String a[])
+	{
+		int sum = 0, power = 0, n1, n2, Nth_num;
+		Number num = new Number();  // Get the Nth position
+		Scanner sc = new Scanner(System.in);
+		Nth_num = sc.nextInt();
+		n1 = 3;
+		n2 = 4;
+		while(flag == 0)
+		{
+			fun(sum, power, n1, n1, n2, Nth_num); //Print the possibilities for 3
+			fun(sum, power, n2, n1, n2, Nth_num); //Print the possibilities for 4
+			power++;
+		}
+		System.out.println();
+	}
+}
