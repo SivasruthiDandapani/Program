13d3bd8f4a6322125114850358e1995171c569e7
SivasruthiDandapani <34651309+SivasruthiDandapani@users.noreply.github.com>
2018-07-02 23:49:06 +0530

    C Programs

---
+++ BattleGround.c
@@ -0,0 +1,131 @@
+﻿/*  QUESTION :
+Battlegrounds
+
+Battlegrounds is a game which has a map denoted by an NxM matrix. The map can have open areas (where players can move) denoted by 0s, 1s where there are people or closed areas (where players can’t move) denoted by Xs. Each player should be able to move around the map and should be able to reach every other player’s location. Consider you are the game’s developer. Given the map you need to find if they all can reach each other. Each player can move up, down, left and right on the map.
+
+Input: First line of the input contains 2 space separated integers N and M.
+
+         Next N lines contain N values that can be either X or 0 denoting the map matrix.
+
+
+Output: Single character Y if all players can reach each other with given moves or N if they can’t.
+ 
+
+Sample Input 1
+
+4 4
+X01X
+10X0
+XX00
+0X0X
+
+Sample Ouput 1
+
+Y
+ 
+
+Sample Input 2
+
+4 4
+X00X
+10X0
+XX00
+0X1X
+
+ 
+Sample Ouput 2
+
+N
+*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int r, c;
+char ch;
+
+
+//Traverse the matrix using backtracking and fill the dummy matrix with 1 if reached
+void Path(char a[r][c],int x, int y, int sol[r][c])
+{
+    if(y != 0 && a[x][y-1] != 'X' && sol[x][y-1] != 1)
+    {
+        sol[x][y-1] = 1;
+        Path(a,x, y-1, sol);
+    }
+    if(y != r-1 && a[x][y+1] != 'X' && sol[x][y+1] != 1)
+    {
+        sol[x][y+1] = 1;
+        Path(a, x, y+1, sol);
+    }
+    if(x != 0 && a[x-1][y] != 'X' && sol[x-1][y] != 1)
+    {
+        sol[x-1][y] = 1;
+        Path(a, x-1, y, sol);
+    }
+    if(x != c-1 && a[x+1][y] != 'X' && sol[x+1][y] != 1)
+    {
+        sol[x+1][y] = 1;
+        Path(a, x+1, y, sol);
+    }
+    
+}
+
+int main() {
+    int i, j, flag = 0, brk = 0;
+    scanf("%d%d", &r, &c);
+    char a[r][c];
+    int sol[r][c]; //Create a dummy matrix to track the path
+    scanf("%c",&ch);
+    for(i = 0; i < r; i++)
+    {
+        
+        for(j = 0; j < c; j++)
+        {
+            sol[i][j] = 0;
+            scanf("%c", &a[i][j]);
+            
+        }
+        scanf("%c",&ch);
+    }
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            if(a[i][j] == '1')
+            {
+                sol[i][j] = 1;
+                Path(a, i, j, sol);  //Call the function that stores the path from this position
+                brk = 1;
+                break;
+            }
+        }
+        if(brk == 1)
+            break;
+    }
+    
+    brk = 0;
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            if(a[i][j] == '1' && sol[i][j] != 1) //Check the original array with the dummy array for all positions
+            {
+                flag = 0;
+                printf("N");
+                brk = 1;
+                break;
+            }
+            else
+            {
+                flag = 1;
+            }
+        }
+        if(brk == 1)
+            break;
+    }
+    if(flag == 1)
+        printf("Y");
+}
---
+++ Counting_min_no_of_Bridges_btw_Rocks.c
@@ -0,0 +1,85 @@
+﻿/* QUESTION
+zeitgeist_baba visits the land of rocks. There are a total of N rocks numbered
+from 1 to N. Some pairs of rocks are connected to each other by Bidirectional
+bridges running over water. zeitgeist_baba hates to cross these bridges as they
+require a lot of efforts. He is standing at rock#1 and wants to reach the
+rock#N. Find the minimum number of bridges that he shall have to cross, if
+he takes the optimal route.
+
+INPUT FORMAT :
+First line contains T. T testcases follow. First line of each test case contains
+two space-separated integers N, M. Each of the next M lines contains two
+space-separated integers X and Y , denoting that there is a bridge between
+RockX and RockY.
+
+CONSTRAINTS : 
+1 <= T <= 10
+1 <= N <= 104
+1 <= M <= 105
+1 <= X, Y <= N
+
+OUTPUT :
+Print the answer to each test case in a new line
+
+SAMPLE I/P :
+2
+3 2
+1 2
+2 3
+4 4
+1 2
+2 3
+3 4
+4 2
+
+SAMPLE O/P :
+2
+2*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int i, j, t, n, m, *a, first, next, cnt = 0;
+    scanf("%d",&t);
+    for(j = 0; j < t; j++)
+    {
+        cnt = 0;
+        scanf("%d %d",&n, &m); //Consider N as no. of rocks
+        a = (int *)malloc((2*m) * sizeof(int));
+        first = 1;  //Take 1 as first rock
+        next = first + 1;
+        for(i = 0; i < 2*m; i+=2)
+        {
+            scanf("%d %d", (a+i), (a+i+1));
+        }
+        //Get the next gratest rock
+        L1 : for(i = 0; i < 2*m; i+=2)
+        {
+            if(*(a+i) == first && *(a+i+1) > next)
+            {
+                next = *(a+i+1);
+            }
+            if(*(a+i+1) == first && *(a+i) > next)
+            {
+                next = *(a+i);
+            }
+        }
+        
+        cnt++;
+        //initialize that to first and iterate it until you reach the last rock
+        if(next != n)
+        {
+            first = next;
+            next = first + 1;
+            goto L1;
+        }
+        else
+        {
+            printf("%d\n",cnt);
+        }
+    }
+    return 0;
+}
+
