c4eaddf0152610c53d51481152be69e984b02283
SivasruthiDandapani <34651309+SivasruthiDandapani@users.noreply.github.com>
2018-07-02 23:35:08 +0530

    Add files via upload

---
+++ CrossPattern.c
@@ -0,0 +1,51 @@
+﻿#include<stdio.h>
+void space(int);
+int j;
+int main() {
+   int i,j,n,count;
+   scanf("%d",&n);
+   char *str;
+   str = (char *)malloc(sizeof(char) * n);
+   scanf("%s",str);
+   printf("String %s\n",str);
+   count = n-1;
+   for(i=1;i<=(n/2)+1;i++)
+   {
+        if(count==0)
+       {
+          space(i-1);
+           printf("%c",*(str+i-1));
+           break;
+       }
+       if(count!=0)
+       {
+           space(i-1);
+           printf("%c",*(str+i-1));
+           space(count);
+           printf("%c",*(str+i-1));
+           count-=2;
+       }
+       printf("\n");
+   }
+   j=(n/2)-1;
+   for(i=(n/2)+2;i<=n;i++)
+   {
+       printf("\n"); 
+       
+        count+=2;
+           space(j--);
+           printf("%c",*(str+i-1));
+           space(count);
+           printf("%c",*(str+i-1));
+           
+       
+       
+   }
+}
+void space(int c)
+{
+    for(j=0;j<c;j++)
+    {
+       printf(".");
+    }
+}
